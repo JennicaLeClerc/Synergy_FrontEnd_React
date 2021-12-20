79fa121df4af77a08f801b62c763d15237ae273d
Jennica LeClerc <jennica@macbook-air.myfiosgateway.com>
2021-12-20 17:34:19 -0500

    tabbing

--- src/createReservation.js
+++ src/createReservation.js
@@ -1,4 +1,5 @@
-import React, {useState} from "react";import ReactDOM from 'react-dom';
+import React, {useState} from "react";
+import ReactDOM from 'react-dom';
 import { 
 	Button,
 	Row,
@@ -67,61 +68,48 @@ const Reservation = ({JWT}) => {
 
 	return(
 		<>
-		<br/><br/><br/>
-		<Container>
-			<Row>
-				<Col></Col>
-				<Col md="auto" className = "text-center">
-				<body className="reserveBody">
-					<form id="reserve" onSubmit={(e) => submit(e)}>
-
-					<div class="elem">
-						<label className="reserveLabel" htmlFor="name">Name</label>
-						
-						<input type="name" id="Name" name="Name"/>	
-					</div>
-					<div class="elem">
-						<label className="reserveLabel" htmlFor="phone">Phone Number</label>
-						
-						<input type="phone" id="phone" name="phone" />
-					</div>
-					<div className="dateElem">
-						<label className="reserveLabel" htmlFor="startDate">Check-in Date</label>
-						
-						<input type="date" id="startDate" className="checkdates" name="startDate" onChange={(e)=> change(e)} required/>	
-					</div>
-					<div className="dateElem">
-						<label className="reserveLabel" htmlFor="endDate">Check-out Date</label>
-						
-						<input type="date" id="endDate" className="checkDates" name="endDate" onChange={(e)=> change(e)} required/>
-					</div>	
-					<div class="elem">
-						<label className="reserveLabel" htmlFor="room-selection">Select Room Preference</label>
-						
-						<select id="room-selection" name="room_preference" required>
-							<option value="">Choose a Room from the List</option>
-							<option value="connecting">1 Bed</option>
-							<option value="adjoining">2 Beds</option>
-							<option value="adjacent">3 Beds</option>
-						</select>
-					</div>
-						
-					<div class="elem">
-						<label className="reserveLabel" htmlFor="message">Anything Else?</label>
-						
-						<textarea id="accommodations" name="accommodations" placeholder="Please make any additional requests here." onChange={(e)=> change(e)}></textarea>
-						
-					</div>
-						<Button style={{ backgroundColor: "#f26926"}} type="submit" value="Submit" >Submit</Button>
-					</form>
-				</body>
-				</Col>
-				<Col></Col>
-			</Row>
-		</Container>
-
-		</>				
-	
+			<br/><br/><br/>
+			<Container>
+				<Row>
+					<Col></Col>
+					<Col md="auto" className = "text-center">
+						<form id="reserve" onSubmit={(e) => submit(e)}>
+							<div className="elem">
+								<label className="reserveLabel" htmlFor="name">Name</label>
+								<input type="name" id="Name" name="Name"/>	
+							</div>
+							<div className="elem">
+								<label className="reserveLabel" htmlFor="phone">Phone Number</label>
+								<input type="phone" id="phone" name="phone" />
+							</div>
+							<div className="dateElem">
+								<label className="reserveLabel" htmlFor="startDate">Check-in Date</label>
+								<input type="date" id="startDate" className="checkdates" name="startDate" onChange={(e)=> change(e)} required/>	
+							</div>
+							<div className="dateElem">
+								<label className="reserveLabel" htmlFor="endDate">Check-out Date</label>
+								<input type="date" id="endDate" className="checkDates" name="endDate" onChange={(e)=> change(e)} required/>
+							</div>	
+							<div className="elem">
+								<label className="reserveLabel" htmlFor="room-selection">Select Room Preference</label>
+								<select id="room-selection" name="room_preference" required>
+									<option value="">Choose a Room from the List</option>
+									<option value="connecting">1 Bed</option>
+									<option value="adjoining">2 Beds</option>
+									<option value="adjacent">3 Beds</option>
+								</select>
+							</div>
+							<div className="elem">
+								<label className="reserveLabel" htmlFor="message">Anything Else?</label>
+								<textarea id="accommodations" name="accommodations" placeholder="Please make any additional requests here." onChange={(e)=> change(e)}></textarea>
+							</div>
+							<Button style={{ backgroundColor: "#f26926"}} type="submit" value="Submit" >Submit</Button>
+						</form>
+					</Col>
+					<Col></Col>
+				</Row>
+			</Container>
+		</>
 	)
 };
 
